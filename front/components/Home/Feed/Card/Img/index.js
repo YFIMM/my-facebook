5bd46e0d70e1f811f09bcdb151586b359590b35e
YFIMM <pushrsp@gmail.com>
2020-12-05 18:19:53 +0900

    img margin

--- front/components/Home/Feed/Card/Img/index.js
+++ front/components/Home/Feed/Card/Img/index.js
@@ -12,9 +12,9 @@ const Img = () => {
   const test = [
     "http://localhost:5003/1.png",
     "http://localhost:5003/2.png",
-    "http://localhost:5003/3.png",
-    "http://localhost:5003/13.png",
-    "http://localhost:5003/14.jpg",
+    // "http://localhost:5003/3.png",
+    // "http://localhost:5003/13.png",
+    // "http://localhost:5003/14.jpg",
   ];
   const imgLen = test.length <= 5 ? test.length : 5;
   return (
@@ -31,24 +31,43 @@ const Img = () => {
             return (
               <EvenBox
                 key={i}
-                width={100 / imgLen === 50 ? "100%" : "50%"}
-                height={100 / imgLen === 25 ? "50%" : "50%"}
+                width={100 / imgLen === 50 ? "100%" : "49.5%"}
+                height={100 / imgLen === 25 ? "49.5%" : "49.5%"}
+                marginLeft={(i === 1 || i === 3) && 100 / imgLen === 25 && "1%"}
+                marginTop={(i === 2 || i === 3) && "1%"}
+                marginBottom={i === 0 && 100 / imgLen === 50 && "1%"}
               >
                 <Image src={v} alt={v} />
               </EvenBox>
             );
           } else if (imgLen === 3) {
             return (
-              <OddBox key={i} width="50%" height={i === 0 ? "100%" : "50%"}>
+              <OddBox
+                marginRight={i === 0 && "1%"}
+                key={i}
+                width="49.5%"
+                height={i === 0 ? "100%" : "49.8%"}
+                marginBottom={(i === 1 || i === 2) && "0.4%"}
+              >
                 <Image src={v} alt={v} />
               </OddBox>
             );
           } else if (imgLen === 5) {
+            let marginRight = 0;
+            if (i === 0) {
+              marginRight = 0.25;
+            } else if (i === 3) {
+              marginRight = 0.5;
+            }
+
             return (
               <OddBox
                 key={i}
-                width={i === 0 || i === 1 ? "50%" : "33%"}
-                height="50%"
+                width={i === 0 || i === 1 ? "49.875%" : "33%"}
+                height="49.875%"
+                marginRight={`${marginRight}%`}
+                marginBottom="0.25%"
+                marginLeft={i === 3 && "0.5%"}
               >
                 <Image src={v} alt={v} />
               </OddBox>
--- front/components/Home/Feed/Card/Img/style.js
+++ front/components/Home/Feed/Card/Img/style.js
@@ -15,12 +15,19 @@ export const OddBox = styled.div`
   width: ${(props) => props.width};
   height: ${(props) => props.height};
   float: left;
+  margin-right: ${(props) => (props.marginRight ? props.marginRight : "0")};
+  margin-bottom: ${(props) => (props.marginBottom ? props.marginBottom : "0")};
+  margin-left: ${(props) => (props.marginLeft ? props.marginLeft : "0")};
 `;
 
 export const EvenBox = styled.div`
   float: left;
   width: ${(props) => props.width};
   height: ${(props) => props.height};
+  margin-right: ${(props) => (props.marginRight ? props.marginRight : "0")};
+  margin-bottom: ${(props) => (props.marginBottom ? props.marginBottom : "0")};
+  margin-left: ${(props) => (props.marginLeft ? props.marginLeft : "0")};
+  margin-top: ${(props) => (props.marginTop ? props.marginTop : "0")};
 `;
 
 export const Image = styled.img`
